fbad8f74e376a1541956fb68651e3f630be5f9f5
Joshua Kisb <joshuakisb@gmail.com>
2020-06-22 16:09:04 +0300

    add basic permissions system

--- app/Role.php
+++ app/Role.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use App\CrudModel;
+use App\Permission;
 use App\DataTypes\Role as RoleDatatype;
 
 class Role extends CrudModel
@@ -12,5 +13,27 @@ class Role extends CrudModel
 	public function getDisplayNameAttribute()
 	{
 		return $this->attributes['display_name'];
+	}
+
+	public function permissions()
+	{
+		return $this->belongsToMany(Permission::class, 'role_permissions');
+	}
+
+	public function addPermission($permission)
+	{
+		if ($permission instanceof Permission)
+			$this->permissions()->save($permission);
+		elseif (is_string($permission)) {
+			$permission = Permission::where('name', $permission)->firstOrFail();
+			$this->permissions()->save($permission);
+		}
+	}
+
+	public function addPermissions($permissions = [])
+	{
+		foreach ($permissions as $permission) {
+			$this->addPermission($permission);
+		}
 	}
 }
